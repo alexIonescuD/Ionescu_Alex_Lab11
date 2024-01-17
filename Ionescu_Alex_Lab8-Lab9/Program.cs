86b8fdacaf41916973fe0b1a84c7a8386745edc8
alexIonescuD <dionescu90@yahoo.com>
2024-01-17 20:39:17 +0200

    Add project files.

---
+++ Ionescu_Alex_Lab8-Lab9/Program.cs
@@ -0,0 +1,89 @@
+using Ionescu_Alex_Lab2.Data;
+using Ionescu_Alex_Lab2.Hubs;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<LibraryContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDbContext<IdentityContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<IdentityContext>();
+
+builder.Services.AddRazorPages();
+builder.Services.AddSignalR();
+
+builder.Services.Configure<IdentityOptions>(options => {
+
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+    options.Lockout.MaxFailedAccessAttempts = 3;
+    options.Lockout.AllowedForNewUsers = true;
+});
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    // Default Password settings.
+    //options.Password.RequireDigit = true;
+   // options.Password.RequireLowercase = true;
+    //options.Password.RequireNonAlphanumeric = true;
+   // options.Password.RequireUppercase = true;
+    options.Password.RequiredLength = 8;
+    //options.Password.RequiredUniqueChars = 1;
+});
+
+builder.Services.AddAuthorization(opts => {
+    opts.AddPolicy("OnlySales", policy => {
+        policy.RequireClaim("Department", "Sales");
+    });
+});
+
+builder.Services.AddAuthorization(opts => { 
+    opts.AddPolicy("SalesManager", policy => { 
+        policy.RequireRole("Manager"); 
+        policy.RequireClaim("Department", "Sales"); }); 
+});
+
+builder.Services.ConfigureApplicationCookie(opts => {
+    opts.AccessDeniedPath = "/Identity/Account/AccessDenied";
+});
+
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    DbInitializer.Initialize(services);
+}
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapHub<ChatHub>("/Chat");
+app.MapRazorPages();
+
+app.Run();
